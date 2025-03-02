db422978c8c666fa64a91f43eacf6a0bed944256
Ahmad Edris Malia <adrismalya@gmail.com>
2025-03-02 13:30:34 +0430

    first commit

---
+++ frontend/src/router/routes.js
@@ -0,0 +1,37 @@
+import { userManagementRoutes } from 'pages/UserManagement/userManagementRoutes'
+import { configurationRoutes } from 'pages/Configurations/configurationRoutes'
+
+const routes = [
+    {
+        path: '/',
+        component: () => import('layouts/MainLayout.vue'),
+        children: [
+            { path: '', component: () => import('pages/IndexPage.vue') },
+            { ...configurationRoutes },
+            { ...userManagementRoutes },
+        ],
+    },
+
+    {
+        path: '/login',
+        component: () => import('pages/LoginPage.vue'),
+    },
+
+    {
+        path: '/server-error',
+        component: () => import('pages/ServerErrorPage.vue'),
+    },
+    {
+        path: '/forbidden',
+        component: () => import('pages/ForbiddenPage.vue'),
+    },
+
+    // Always leave this as last one,
+    // but you can also remove it
+    {
+        path: '/:catchAll(.*)*',
+        component: () => import('pages/ErrorNotFound.vue'),
+    },
+]
+
+export default routes
